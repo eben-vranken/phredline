d379300d2dd8ef7205eb1393b25b1519dd00549b
eben-vranken <vranken.eben@outlook.com>
2026-06-29 21:47:33 +0200

    feat: add line splitter yielding invididual lines as byte sequences

--- src/phredline/parser.py
+++ src/phredline/parser.py
@@ -11,15 +11,33 @@ def read_chunks(file_path, chunk_size):
             else:
                 yield bchunk
 
+def read_lines(chunk_stream):
+    tail = b""
+
+    for chunk in chunk_stream:
+        combined_data = tail + chunk
+
+        chunk_split = combined_data.split(b"\n")
+
+        for split in chunk_split[:-1]:
+            yield split
+
+        tail = chunk_split[-1]
+
+        pass
+ 
+    if len(tail) > 0:
+        yield tail
 
 if __name__ == "__main__":
-     print("--- Chunk Reader ---")
+    print("--- Chunk Reader ---")
 
-     chunk_stream = read_chunks(FILE_PATH, CHUNK_SIZE)
+    chunks = read_chunks(FILE_PATH, CHUNK_SIZE)
+    lines = read_lines(chunks)
 
-     for i, chunk in enumerate(chunk_stream):
-         print(f"Chunk {i}: {chunk} (Length: {len(chunk)})")
+    for i, chunk in enumerate(lines):
+        print(f"Chunk {i}: {chunk} (Length: {len(chunk)})")
 
-         if i >= 100:
-            print("Stopping test early")
-            break
+        if i >= 100:
+           print("Stopping test early")
+           break
